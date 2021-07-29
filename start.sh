5e582ea5191c4c2e2327f84b9a8ea02c21f08eb4
Santeri Hetekivi <santeri@hetekivi.com>
2021-07-29 22:08:49 +0300

    Fix start scrip.

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+# Compose file path.
+COMPOSE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/docker-compose.yaml"&&
 # Start compose.
-docker-compose up -d &&
+docker-compose -f $COMPOSE_PATH up -d &&
 # Add to cron.
-echo "00 09 * * * docker compose up mal_list_downloader" > ./mycron &&
+echo "00 09 * * * docker-compose -f $COMPOSE_PATH up mal_list_downloader" > ./mycron &&
 crontab ./mycron &&
 rm ./mycron &&
 echo "done"
