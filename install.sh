5e0653d8bb124c7a8ac2595b5c8a9d7c3580fb2d
Santeri Hetekivi <SanteriHetekivi@users.noreply.github.com>
2021-09-15 15:28:17 +0300

    Docker compose version 1.29.2

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - &&
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" &&
 sudo apt update &&
 sudo apt -y install docker-ce &&
-sudo curl -L https://github.com/docker/compose/releases/download/1.28.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 sudo usermod -aG docker ${USER} &&
 su - ${USER}
